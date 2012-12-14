07d5c0c8e3993a30925ad4786caf5f32fbf1b81e
Phillip Tarrant <ptarrant@gmail.com>
2012-12-14 12:49:26 -0500

    fixed

--- app/pages/main_page.php
+++ app/pages/main_page.php
@@ -1,1 +1,6 @@
-our front page, this is just a placeholder
+our front page, this is just a placeholder<br />
+<br />
+Zippy,<br />
+if you want, go ahead and do your template here.<br />
+I'll have the $data array for you later, lots of backend code to do first.<br />
+However, we are getting closer to having an alpha product.<br />
