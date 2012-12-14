a32dd1319eb4fef76ee06098258db9e50210f73a
Phillip Tarrant <ptarrant@gmail.com>
2012-12-13 21:38:34 -0500

    Initial Commit

---
+++ system/core/pf_config.php
@@ -0,0 +1,18 @@
+<?php if (!defined('APP_NAME')) die('NO DIRECT SCRIPT ACCESS!');
+
+abstract class pf_config
+{
+    private static $config = array();       //array to hold our config
+    
+    public static function get($setting)
+    {
+        //if the setting exist, we return it, else we return false
+        return array_key_exists($name, self::$data) ? self::$data[$name] : FALSE;
+    }
+    
+    public static function set($name,$setting)
+    {
+        self::$config[$name]=$setting;
+    }
+}
+?>
