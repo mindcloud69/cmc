8617dba462e322e5a1d3d4eb5710fa55120f5636
ptarrant <ptarrant@gmail.com>
2012-12-14 10:08:20 -0500

    starting to use the server_conf class

--- app/controllers/main.php
+++ app/controllers/main.php
@@ -1,14 +1,23 @@
 <?php
 
-class main
+class main extends pf_controller
 {
     public function index()
     {
-        //check if logged in, if not redirect to /login
-        pf_auth::checkLogin('login');
-        //load our main page
+        //check if logged in
+        $this->checkLogin();
         
-        pf_core::loadPage('main');
+        $this->loadLibrary('server_conf');
+        //grab the servers config
+        if (!server_conf::grabConfig('/bukkit/server.properties')) //@todo:make dynamic from settings.json file
+        {
+            pf_events::dispayFatal('Server.properties Not Found!');
+        }
+        
+        
+        //load our main page
+        $this->loadView('main_page');
+        
     }
     
 }
