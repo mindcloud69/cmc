72f4d1a7fd402b97de3e20c1ef68ab7114ad79c7
Phillip Tarrant <ptarrant@gmail.com>
2012-12-13 21:18:39 -0500

    added default_controller added default_method added some comments added environment setting

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,12 @@
 <?php
 /* =============================================================================
- * Config.php - Configuration of the app
+ * CONFIG.PHP - Configuration of the app
  * ===========================================================================*/
 $config = array();
 
-
+/* =============================================================================
+ * APP SPECIFIC - Configuration of the app
+ * ===========================================================================*/
 //Our Timezone
 $config['timezone']     ='America/New_York';
 
@@ -19,13 +21,25 @@ $config['description']  ='A php app to control your minecraft serer';
 $config['shortcut_icon']='icon.gif';
 $config['creation_date']='2012';
 
-//set to any subfolder you have
+/* =============================================================================
+ * ENVIRONMENT - Configuration of the app
+ * ===========================================================================*/
+//set to any subfolder you have if not using one set to ''
 $config['base_url']	= 'CMC'; 
 
 //if  using mod_rewrite set this to ''
 $config['index_page']   = 'index.php'; 
 
 //this should be set to "DEV" or "LIVE"
-$config['environment']  = 'DEV'
+$config['environment']  = 'DEV';
 
+
+/* =============================================================================
+ * ROUTER SETUP - Configuration of the app
+ * ===========================================================================*/
+//our default controller
+$config['default_controller']= 'main';
+
+//our default method
+$config['default_method']= 'index';
 ?>
