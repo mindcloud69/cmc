71f101c01998f2ce083309062d354048724c7450
ptarrant <ptarrant@gmail.com>
2012-12-14 09:33:44 -0500

    Initial Commit

--- app/controllers/data.php
+++ app/controllers/data.php
@@ -5,8 +5,16 @@
  * "model" to get / store data
  */
 
-class data
+class data extends pf_controller
 {
-    
+    public function onlinestatus()
+    {
+        if ($this->loadLibrary('server_conf.php'))
+        {
+        if (server_conf::checkOnline()) echo 'Online';
+        else echo "Offline";
+        }
+        
+    }
 }
 ?>
