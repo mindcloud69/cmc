290165175eba7c65de853d67a82c7e209dd2b529
Phillip Tarrant <ptarrant@gmail.com>
2012-12-16 00:00:41 -0500

    added variables from server_config

--- app/controllers/main.php
+++ app/controllers/main.php
@@ -8,16 +8,48 @@ class main extends pf_controller
         $this->checkLogin();
         
         $this->loadLibrary('server_conf');
+        
         //grab the servers config
-        /*
-        if (!server_conf::grabConfig('/bukkit/server.properties')) //@todo:make dynamic from settings.json file
+        $data = array();
+        $settings = new pf_json();
+        $settings->readJsonFile(APPLICATION_DIR.'config'.DS.'settings.json');
+        
+        $data['bukkit_dir'] = $settings->get('bukkit_dir');
+        
+        
+        if (!server_conf::grabConfig($data['bukkit_dir'].'/server.properties'))
         {
             pf_events::dispayFatal('Server.properties Not Found!');
         }
-        */
+        
+        //get some info from the config file
+        $data['online']=server_conf::checkOnline();
+        $data['world'] = server_conf::getSetting('level-name');
+        $data['pvp'] = server_conf::getSetting('pvp');
+        $data['difficulty'] = server_conf::getSetting('difficulty');
+        $essentials = server_conf::checkEssentials($data['bukkit_dir']);
+
+        $gamemode= server_conf::getSetting('gamemode');
+        if ($gamemode == 0) $gamemode = 'Survival';
+        elseif ($gamemode == 1) $gamemode = 'Creative';
+        elseif ($gamemode == 1) $gamemode = 'Adventure';
+        
+        if ($essentials) $data['essentials']='TRUE';
+        else $data['essentials'] = 'FALSE';
+        
+        $data['gamemode'] = $gamemode;
+        
+        $list = '';
+        server_conf::checkPluggins($data['bukkit_dir']);
+        foreach (server_conf::$pluggins as $plugin)
+        {
+            $list .= $plugin;
+        }
+        
+        $data['pluggins']=$list; //@TODO: FIND OUT WHY NOT REPORTING!
 
         //load our main page
-        $this->loadView('main_page');
+        $this->loadView('main_page',$data);
         
     }
 }
