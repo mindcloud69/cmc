205af7e476258235a0df397d42d58fb94e071bbc
Phillip Tarrant <ptarrant@gmail.com>
2012-12-15 21:59:21 -0500

    New Menu Template Dynamic Login/Logout link Dynamic "active" class depending on location

--- app/templates/menu.php
+++ app/templates/menu.php
@@ -1,10 +1,27 @@
- <?php 
-pf_menu::addTopLevel('Home', 'main/index');
-pf_menu::addTopLevel('Server Config', 'server/config');
-pf_menu::addTopLevel('Players', 'main/players');
+<?php
+//get the active URL
+$url = $_SERVER['REQUEST_URI'];
+$parts = explode('.php', $url);
+$page = end($parts);
 
-if (pf_auth::checkLogin()) pf_menu::addTopLevel('Logout', 'login/logout');
-else pf_menu::addTopLevel('Login', 'login');
-
-pf_menu::renderMenu('dropdown dropdown-linear');
+//check to see if the user is logged in
+if (pf_auth::checkLogin()===TRUE) //if logged in, we show a "logout" button
+{
+    $loginlink = '<a href="'.pf_config::get('base_url').'index.php/login/logout">Logout</a>';
+}
+else //if not logged in, we show a login link :)
+{
+    $loginlink = '<a href="'.pf_config::get('base_url').'index.php/login">Login</a>';
+}
 ?>
+
+<div class="navbar"><!--START NAV-->
+        <div class="navbar-inner">
+                <a class="brand" href="<?php echo pf_config::get('base_url'); ?>index.php">CMC</a>
+                <ul class="nav">
+                        <li <?php if ($page == '') echo 'class="active"'; ?> ><a href="<?php echo pf_config::get('base_url'); ?>index.php">Home</a></li>
+                        <li <?php if ($page == '/install') echo 'class="active"'; ?> ><a href="<?php echo pf_config::get('base_url'); ?>index.php/install">Install/Config</a></li>
+                        <li <?php if ($page == '/login') echo 'class="active"'; ?>> <?php echo $loginlink; ?> </li> <!-- dynamic login/logout link-->
+                </ul>
+        </div>
+</div><!-- END NAV -->
