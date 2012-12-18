688d02942a9789b5e6b3fe6a1148ca83b0de2a27
Phillip Tarrant <ptarrant@gmail.com>
2012-12-18 08:40:12 -0500

    fixed some menu issues. git issues aswell

--- app/templates/menu.php
+++ app/templates/menu.php
@@ -14,21 +14,34 @@ else //if not logged in, we show a login link :)
     $loginlink = '<a href="'.pf_config::get('main_page').'/login">Login</a>';
 }
 
+//check to see if we are installed
+$data = new pf_json();
+$data->readJsonFile(APPLICATION_DIR.'config'.DS.'settings.json');  //grab data from json
+$installed = $data->get('bukkit_dir'); //do we have a bukkit_dir?
 
-//links are as so:
-//basically $page will be == to anything after index.php so if there is nothing after it, it's the home page
-//if there is /install after index.php then $page will be /install
-//if the page == the condition, it echos (prints) the 'class=active' inside the <li> tag. :)
 ?>
 
 <div class="navbar"><!--START NAV-->
         <div class="navbar-inner">
-                <a class="brand" href="<?php echo pf_config::get('base_url'); ?>index.php">CMC</a>
+                <a class="brand" href="<?php echo pf_config::get('main_page'); ?>">CMC</a>
                 <ul class="nav">
+                        <!--Home-->
                         <li <?php if ($page == '') echo 'class="active"'; ?> ><a href="<?php echo pf_config::get('main_page'); ?>">Home</a></li>
+                    
+                        <!--Dynamic Install Link-->
+                        <?php if (!$installed) {?>
                         <li <?php if ($page == '/install') echo 'class="active"'; ?> ><a href="<?php echo pf_config::get('main_page'); ?>/install">Install</a></li>
+<<<<<<< HEAD
                         <li <?php if ($page == '/server') echo 'class="active"'; ?> ><a href="<?php echo pf_config::get('main_page'); ?>/server">Config</a></li>
                         <li <?php if ($page == '/login') echo 'class="active"'; ?>> <?php echo $loginlink; ?> </li> <!-- dynamic login/logout link-->
+=======
+                        <?php }?> 
+                        
+                        <li <?php if ($page == '/server') echo 'class="active"'; ?> ><a href="<?php echo pf_config::get('main_page'); ?>/server">Config</a></li>
+                        
+                        <!--dynamic login link-->
+                        <li <?php if ($page == '/login') echo 'class="active"'; ?>> <?php echo $loginlink; ?> </li>
+>>>>>>> Made Install Link Dynamic
                 </ul>
         </div>
 </div><!-- END NAV -->
