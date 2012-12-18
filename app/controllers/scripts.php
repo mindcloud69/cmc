ff27ea4e9d92e0b45342a0946a8e64a72df513b5
Phillip Tarrant <ptarrant@gmail.com>
2012-12-18 15:19:50 -0500

    Updating changelog

--- app/controllers/scripts.php
+++ app/controllers/scripts.php
@@ -1,7 +1,12 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+class scripts
+{
+    
+    public function start()
+    {
+        
+    }
+
+}
 ?>
--- app/pages/scripts/start_page.php
+++ app/pages/scripts/start_page.php
@@ -1,7 +1,28 @@
-<?php
+<h1>Start Server Script</h1>
+<?php 
+pf_forms::createForm('startserver', null, pf_config::get('main_page')."scripts/start", 'POST');
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 ?>
+<form id="startserver">
+    <select name="startram">
+        <options value="1024">1Gb Memory</option>
+        <options value="2048">2Gb Memory</option>
+        <options value="3072">3Gb Memory</option>
+        <options value="4096">4Gb Memory</option>
+        <options value="5120">5Gb Memory</option>
+        <options value="6144">6Gb Memory</option>
+        <options value="7168">7Gb Memory</option>
+        <options value="8192">8Gb Memory</option>
+    </select>
+    <select name="maxram">
+        <options value="1024">1Gb Memory</option>
+        <options value="2048">2Gb Memory</option>
+        <options value="3072">3Gb Memory</option>
+        <options value="4096">4Gb Memory</option>
+        <options value="5120">5Gb Memory</option>
+        <options value="6144">6Gb Memory</option>
+        <options value="7168">7Gb Memory</option>
+        <options value="8192">8Gb Memory</option>
+    </select>
+    
+</form>
