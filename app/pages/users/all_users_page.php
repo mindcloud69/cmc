71473019699204df3bc48d194402374bbf39d20e
1001zippy <1001zippy@gmail.com>
2012-12-19 19:48:04 -0500

    icons, finally! woot!

--- app/pages/users/all_users_page.php
+++ app/pages/users/all_users_page.php
@@ -1,26 +1,36 @@
-<?php
+<html>
+	<head>
+	
+	
+	</head>
+	<body>
+		<?php
 
-$table = new pf_tables();
+			$table = new pf_tables();
         
-//create some pretty tables
-$table->startTable('users');
-$table->startRow();
-$table->addCell('ID');
-$table->addCell('Name');
-$table->addCell('Level');
-$table->addCell('Delete');
-$table->endRow();
+			//create some pretty (?) tables
+			$table->startTable('users');
+			$table->startRow();
+			$table->addCell('ID');
+			$table->addCell('Name');
+			$table->addCell('Level');
+			$table->addCell('Delete');
+			$table->endRow();
 
-foreach ($data as $user)
-{
-    $table->startRow();
-    $table->addCell($user['ID']);
-    $table->addCell($user['User']);
-    $table->addCell($user['Level']);
-    $table->addCell('<a href="'.pf_config::get('main_page').'/users/delete?id='.$user['ID'].'">Delete </a>');
-    $table->endRow();
-}
+			foreach ($data as $user)
+			{
+    			$table->startRow();
+    			$table->addCell($user['ID']);
+   				$table->addCell($user['User']);
+    			$table->addCell($user['Level']);
+    			$table->addCell('<a href="'.pf_config::get('main_page').'/users/delete?id='.$user['ID'].'">Delete </a>');
+    			$table->endRow();
+			}
 
-$table->renderTable();
-?>
-<a href="<?php echo pf_config::get('main_page');?>/users/add">Add A User</a>
+			$table->renderTable();
+		?>
+		
+		<a href="<?php echo pf_config::get('main_page');?>/users/add">Add A User</a>
+
+	</body>
+</html>
