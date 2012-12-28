1846e38cedec3a30471f8d714008fe02c9a64474
Phillip Tarrant <ptarrant@gmail.com>
2012-12-28 00:25:50 -0500

    Issue #24 Backup Section

---
+++ app/pages/backups/backup_complete_page.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+            <?php pf_core::loadTemplate('header'); ?>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+        <body>
+        <?php pf_core::loadTemplate('menu'); ?>
+            <h3 class="center">Backup Management</h3>
+                        
+            <div class="container">
+                <div class="row">
+                    <div class="valid span6 offset3">
+                        Backup Complete!
+                    </div>
+                </div>
+            </div>
+            <?php pf_core::loadTemplate('footer'); ?>
+    </body>
+</html>
